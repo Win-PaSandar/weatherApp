bf55bf00dc099018a882588297b6b6bef66e02ff
Win Pa <winpasandar1985@gmail.com>
2023-05-04 02:38:06 +0630

    Added search.js.

--- src/search-engine.js
+++ src/search-engine.js
@@ -139,3 +139,5 @@ fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
 
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
+
+search("Yangon");
