57194ad23c590b2c2c01f2082f0573a7c442d2c8
Win Pa <winpasandar1985@gmail.com>
2023-05-10 00:54:01 +0630

    Updated html and js.

--- src/search-engine.js
+++ src/search-engine.js
@@ -54,6 +54,14 @@ let timeElement = document.querySelector("#current-time");
 let currentTime = new Date();
 timeElement.innerHTML = formatTime(currentTime);
 
+function retrievePosition(position) {
+  let lat = position.coordinates.latitude;
+  let lon = position.coordinates.longitude;
+  let apiKey = "1acbot392864443e815e617a5f81390f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${lon}&lat=${lat}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showWeather);
+}
+
 function showWeather(response) {
   let cityName = document.querySelector("h1");
   cityName = response.data.city;
@@ -84,10 +92,46 @@ function showWeather(response) {
     );
 }
 
-function searchCity(city) {
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay) {
+    forecastHTML =
+      forecastHTML +
+      `
+      <div class="col-2">
+        <div class="forecast-box">
+          <div class="forecast-day">${forecastDay.dt}</div>
+            <img
+              src= "https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+                forecastDay.condition.icon
+              }.png" 
+              alt=""              
+              class="forecast-icon"
+            />
+          <div>
+          <span class="forecast-max">${Math.round(
+            forecastDay.temperature.maximum
+          )}&deg</span>
+          <span class="forecast-min">${Math.round(
+            forecastDay.temperature.minimum
+          )}&deg</span>
+          </div>
+        </div>
+      </div>
+        `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  console.log(coordinates);
   let apiKey = "1acbot392864443e815e617a5f81390f";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(showWeather);
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function search(event) {
@@ -97,22 +141,14 @@ function search(event) {
   searchCity(cityInput);
 }
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
-
-function retrievePosition(position) {
-  let lat = position.coordinates.latitude;
-  let lon = position.coordinates.longitude;
-}
-navigator.geolocation.getCurrentPosition(retrievePosition);
-
-function searchLocation(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
+function searchCity(city) {
   let apiKey = "1acbot392864443e815e617a5f81390f";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=imperial`;
-
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(showWeather);
+}
+
+function returnCity(event) {
+  navigator.geolocation.getCurrentPosition(retrievePosition);
 }
 
 function showFahrenheitTemperature(event) {
@@ -131,6 +167,9 @@ function showCelsiusTemperature(event) {
   let celsiusTemperature = ((fahrenheitTemperature - 32) * 5) / 9;
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
 
 let fahrenheitTemperature = null;
 
