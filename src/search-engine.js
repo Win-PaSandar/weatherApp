b74e4b0246ad14b7a184d0f15e801d01bef89e5c
Win Pa <winpasandar1985@gmail.com>
2023-05-10 22:46:30 +0630

    Updated html and js.

--- src/search-engine.js
+++ src/search-engine.js
@@ -69,12 +69,6 @@ function showWeather(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     fahrenheitTemperature
   );
-  document.querySelector("#low-temp").innerHTML = Math.round(
-    response.data.temperature.minimum
-  );
-  document.querySelector("#high-temp").innerHTML = Math.round(
-    response.data.temperature.maximum
-  );
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
@@ -90,6 +84,16 @@ function showWeather(response) {
       "src",
       `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`
     );
+
+  getForecast(response.data.coordinates);
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+  return days[day];
 }
 
 function displayForecast(response) {
@@ -97,13 +101,14 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
 
-  forecast.forEach(function (forecastDay) {
-    forecastHTML =
-      forecastHTML +
-      `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
         <div class="forecast-box">
-          <div class="forecast-day">${forecastDay.dt}</div>
+          <div class="forecast-day">${formatDay(forecastDay.time)}</div>
             <img
               src= "https://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
                 forecastDay.condition.icon
@@ -122,16 +127,22 @@ function displayForecast(response) {
         </div>
       </div>
         `;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "1acbot392864443e815e617a5f81390f";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}`;
   axios.get(apiUrl).then(displayForecast);
+}
+
+function searchCity(city) {
+  let apiKey = "1acbot392864443e815e617a5f81390f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(showWeather);
 }
 
 function search(event) {
@@ -139,12 +150,6 @@ function search(event) {
   let cityInput = document.querySelector("#city-input").value;
   document.querySelector("#current-city").innerHTML = cityInput;
   searchCity(cityInput);
-}
-
-function searchCity(city) {
-  let apiKey = "1acbot392864443e815e617a5f81390f";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-  axios.get(apiUrl).then(showWeather);
 }
 
 function returnCity(event) {
@@ -179,4 +184,4 @@ fahrenheitLink.addEventListener("click", showFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemperature);
 
-search("Yangon");
+searchCity("Yangon");
